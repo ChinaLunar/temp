24a5bec6d4c0e7ab3e2046dc3d5e65e82a758b90
ChinaLunar <551179786@qq.com>
2018-12-08 23:39:40 +0800

    Add files via upload

---
+++ Pair.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+namespace OpsHelper.Util.Config
+{
+    class Pair
+    {
+        #region 成员变量
+
+        private String key = "";
+
+        private String value = "";
+
+        private bool empty = true;
+
+        #endregion
+
+        public Pair(String key, String value)
+        {
+            if (null == key || null == value)
+            {
+                throw new NullReferenceException();
+            }
+
+            if (String.Empty != key)
+            {
+                this.key = key;
+                this.value = value;
+                this.empty = false;
+            }
+        }
+
+        public Pair setKey(String key)
+        {
+            if (null == key)
+            {
+                throw new NullReferenceException();
+            }
+
+            this.key = key;
+
+            if (String.Empty == key)
+            {
+                this.value = "";
+                this.empty = true;
+            }
+
+            return this;
+        }
+
+        public String getKey()
+        {
+            return this.key;
+        }
+
+        private Pair setValue(String value)
+        {
+            if (null == value)
+            {
+                throw new NullReferenceException();
+            }
+
+            this.value = value;
+
+            return this;
+        }
+
+        public String getValue()
+        {
+            return this.value;
+        }
+
+        public bool isEmpty()
+        {
+            return this.empty;
+        }
+    }
+}
---
+++ PropsConfig.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace OpsHelper.Util.Config
+{
+    class PropsConfig
+    {
+        #region 成员变量
+
+        private Dictionary<String, String> props = new Dictionary<String, String>(10);
+
+        #endregion
+
+
+        public PropsConfig(){
+            String filename = @"C:\Users\55117\.opshelper\temp.txt";
+
+            loadPropsConfig(filename);
+        }
+
+        private bool loadPropsConfig(String filename)
+        {
+            using (StreamReader reader = new StreamReader(filename))
+            {
+                String temp = "";
+
+                do
+                {
+                    if (String.Empty != temp)
+                    {
+                        Pair entry = PropsConfig.parsePair(temp, "=");
+                        if (!entry.isEmpty())
+                        {
+                            this.props.Add(entry.getKey(), entry.getValue());
+                        }
+                    }
+
+                    temp = reader.ReadLine();
+                } while (temp != null);
+            }
+
+            return true;
+        }
+
+        private static Pair parsePair(String src, String gene)
+        {
+            int index = src.IndexOf(gene);
+
+            if (index >= 0)
+            {
+                String key = src.Substring(0, index).Trim();
+                String value = src.Substring(index, src.Length - index).Trim();
+                return new Pair(key, value);
+            }
+
+            return new Pair("", "");
+        }
+
+        private String getProp(String key)
+        {
+            if (null == key)
+            {
+                throw new NullReferenceException();
+            }
+
+            if (this.props.ContainsKey(key))
+            {
+                return this.props[key];
+            }
+            else
+            {
+                return "";
+            }
+
+        }
+
+        private PropsConfig setProps(String key, String value)
+        {
+            if(null == key)
+            {
+                throw new NullReferenceException();
+            }
+
+            if(String.Empty != key)
+            {
+                if (this.props.ContainsKey(key))
+                {
+                    this.props[key] = value;
+                }
+                else
+                {
+                    this.props.Add(key, value);
+                }
+            }
+
+            return this;
+        }
+    }
+}
